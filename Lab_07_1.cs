d90f4aa841ad5f6568588108b144c5cebb9f5979
SodAcetate <sodium.acetate.82@gmail.com>
2022-10-27 14:38:31 +0300

    init commit

---
+++ Lab_07_1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lab_07_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
